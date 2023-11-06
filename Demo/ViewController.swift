763582b1f85a9a861ba30fc5246328d3659142eb
nevzorov46 <masterpiece.nevzorov@gmail.com>
2023-11-06 14:25:45 +0300

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Admin on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
